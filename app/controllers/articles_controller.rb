3b7ed0b22121c360edb0fdaede61a4c8c0af10f3
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2018-10-09 17:12:21 +0200

    example of image upload in ajax

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update]
+
   def index
     @articles = Article.all
   end
@@ -17,9 +19,31 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      respond_to do |format|
+        format.html { redirect_to edit_article_path(@article) }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { render :edit }
+        format.js
+      end
+    end
+
+  end
+
   private
 
   def article_params
     params.require(:article).permit(:title, :content, :photo)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
 
-  resources :articles, only: [:index, :new, :create]
+  resources :articles, only: [:index, :new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
